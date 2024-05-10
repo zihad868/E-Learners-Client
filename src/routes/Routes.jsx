8c5cf182d43b094b7211e2460ffeab59cd247a72
zihad868 <zihadh868@gmail.com>
2024-05-10 12:49:35 +0600

    Rect router setup

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
+    }
+])
+
+export default router;
