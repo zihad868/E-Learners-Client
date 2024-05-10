9d37febafdbec5fe82744b97b4692f00dfedab47
zihad868 <zihadh868@gmail.com>
2024-05-10 12:13:41 +0600

    Setup Tailwind and DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+      <h3 className="text-3xl">Library management System</h3>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
