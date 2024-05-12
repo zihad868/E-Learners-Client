3bf240a2546093d1c370dd1ea7c807b677397333
zihad868 <zihadh868@gmail.com>
2024-05-12 22:53:36 +0600

    Firebase env configuration

--- src/firebase/Firebase.Config.js
+++ src/firebase/Firebase.Config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyACYTwrb8ND2k3pawfSWDXwdvYzp70ddLE",
-  authDomain: "librarymanagement-eed8e.firebaseapp.com",
-  projectId: "librarymanagement-eed8e",
-  storageBucket: "librarymanagement-eed8e.appspot.com",
-  messagingSenderId: "488681716459",
-  appId: "1:488681716459:web:9b8f46329003b781eddcae"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -1,10 +1,137 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
 
 const AddBook = () => {
+    const { user } = useContext(AuthContext);
+    const [selectCategory, setCategory] = useState();
+
+    const handleCategory = (e) => {
+        setCategory(e.target.value)
+    }
+
+    const handleAddBook = (e) => {
+        e.preventDefault();
+
+        const photo = e.target.photo.value;
+        const name = e.target.name.value;
+        const quantity = e.target.quantity.value;
+        const authName = e.target.authName.value;
+        const rating = e.target.rating.value;
+        const category = selectCategory;
+        const description = e.target.description.value;
+        const email = user?.email;
+        
+        const addBook = {photo, name, quantity, authName, category, description, rating, email};
+
+        console.log(addBook)
+    }
+
     return (
         <div>
-            <h5>Add Book</h5>
-        </div>
+         <h2 className="text-center font-bold text-2xl text-secondary space-y-3">Add Book</h2>
+        <form onSubmit={handleAddBook} className="w-3/4 mx-auto border-2 mt-3">
+          <div className="grid sm:grid-cols-1 lg:grid-cols-2 mt-5">
+            <div className="p-3">
+              <label className="label">
+                <span className="label-text">Book Image</span>
+              </label>
+              <input
+                type="text"
+                name="photo"
+                placeholder="Book Photo URL"
+                className="input input-bordered w-full"
+              />
+            </div>
+
+            <div className="p-3">
+              <label className="label">
+                <span className="label-text">Book Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Book Name"
+                className="input input-bordered w-full"
+              />
+            </div>
+
+          </div>
+
+          <div className="p-3">
+              <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <input
+                type="text"
+                name="description"
+                placeholder="Book Description"
+                className="input input-bordered w-full"
+              />
+            </div>
+  
+          <div className="grid sm:grid-cols-1 lg:grid-cols-2 mt-5">
+            <div className="p-3">
+              <label className="label">
+                <span className="label-text">Book Quantity </span>
+              </label>
+              <input
+                type="number"
+                name="quantity"
+                placeholder="Book Quantity"
+                className="input input-bordered w-full"
+              />
+            </div>
+  
+            <div className="p-3">
+              <label className="label">
+                <span className="label-text">Author Name</span>
+              </label>
+              <input
+                type="text"
+                name="authName"
+                placeholder="Author Name"
+                className="input input-bordered w-full"
+              />
+            </div>
+  
+            <div className="p-3">
+              <label className="label">
+                <span className="label-text">Category</span>
+              </label>
+              <select className="select w-full" onChange={handleCategory}>
+                <option disabled selected>
+                  Select Category
+                </option>
+                <option value='Novel'>Novel</option>
+                <option value='Thriller'>Thriller</option>
+                <option value='History'>History</option>
+                <option value='Drama'>Drama</option>
+                <option value='Sci-Fi'>Sci-Fi</option>
+              </select>
+            </div>
+  
+  
+            <div className="p-3">
+              <label className="label">
+                <span className="label-text">Rating</span>
+              </label>
+              <input
+                type="number"
+                name="rating"
+                min={1}
+                max={5}
+                placeholder="Rating"
+                className="input input-bordered w-full"
+              />
+            </div>
+  
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-secondary">Add Book</button>
+          </div>
+        </form>
+      </div>
     );
-};
+  };
 
 export default AddBook;
