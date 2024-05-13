0aa17925751a1bd5bcdb50a34b69e21b9c7bf503
zihad868 <zihadh868@gmail.com>
2024-05-13 14:23:09 +0600

    Delete Book API Call

--- src/components/BooksCard.jsx
+++ src/components/BooksCard.jsx
@@ -1,10 +1,41 @@
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import Swal from 'sweetalert2';
 
-const BooksCard = ({ book }) => {
+const BooksCard = ({ book, setIsDeleted, isDeleted }) => {
     const {_id, photo, name, quantity, category, description, rating} = book;
 
-    console.log(book)
+    const handleDelete = (id) => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "Want to remove book!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            fetch(`${import.meta.env.VITE_API_URL}/deleteBook/${id}`, {
+              method: "DELETE",
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                if (data.deletedCount == 1) {
+                  Swal.fire({
+                    position: "top",
+                    icon: "success",
+                    title: "Book Successfully Removed",
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+                  setIsDeleted(!isDeleted);
+                }
+              });
+          }
+        });
+      };
+    
     return (
         <div  className='space-y-3 mb-10'>
             <div className='flex justify-center p-3'>
@@ -28,7 +59,7 @@ const BooksCard = ({ book }) => {
             <div className='grid grid-cols-3 gap-2'>
               <Link to={`/books/${_id}`}><button className='bg-sky-300 p-2 rounded-md w-full'>View Details</button></Link>
               <Link to={`/bookUpdate/${_id}`}><button className='bg-purple-400 p-2 rounded-md w-full'>Update</button></Link>
-              <button className='bg-red-400 p-2 rounded-md w-full'>Delete</button>
+              <button onClick={() => handleDelete(_id)} className='bg-red-400 p-2 rounded-md w-full'>Delete</button>
             </div>
         </div>
     );
--- src/pages/AllBooks.jsx
+++ src/pages/AllBooks.jsx
@@ -3,19 +3,22 @@ import BooksCard from "../components/BooksCard";
 
 const AllBooks = () => {
     const [books, setBooks] = useState([])
+    const [isDeleted, setIsDeleted] = useState(false);
+
     useEffect(() => {
         fetch(`${import.meta.env.VITE_API_URL}/books`)
           .then(res => res.json())
           .then(data => {
             setBooks(data)
           })
-    }, [])
+    }, [isDeleted])
+
     return (
         <div>
             <h4 className="text-center font-bold text-2xl text-secondary space-y-3">All Books</h4>
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
-                    books.map(book => <BooksCard key={book._id} book={book} />)
+                    books.map(book => <BooksCard key={book._id} book={book} setIsDeleted={setIsDeleted} isDeleted={isDeleted}/>)
                 }
             </div>
         </div>
