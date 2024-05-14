6ae746e0ee1234f9b909c41e69a7903d2b1de401
zihad868 <zihadh868@gmail.com>
2024-05-14 17:42:50 +0600

    Updated

--- src/components/BorrowCard.jsx
+++ src/components/BorrowCard.jsx
@@ -1,43 +1,42 @@
-import PropTypes from 'prop-types';
-import Swal from 'sweetalert2'
+import PropTypes from "prop-types";
+import Swal from "sweetalert2";
 
-const BorrowCard = ({ borrow }) => {
-
-    const handleReturn = (id, borrowId) => {
-
-        fetch(`${import.meta.env.VITE_API_URL}/books/${id}`)
-        .then(res => res.json())
-        .then(data => {
-            fetch(`${import.meta.env.VITE_API_URL}/bookReduce/${id}`,{
-                method: 'PATCH',
-                headers: {
-                    'content-type': 'application/json'
-                 },
-                body: JSON.stringify(data)
-              })
-              .then(res => res.json())
-              .then(data => {
-                console.log(data)
-                console.log(borrowId)
-                fetch(`${import.meta.env.VITE_API_URL}/deleteBorrow/${borrowId}`, {
-                    method: 'DELETE'
-                })
-                 .then(res => res.json())
-                 .then(data => {
-                    console.log(data)
-                    if (data.deletedCount === 1) {
-                        Swal.fire({
-                            position: "top-end",
-                            icon: "success",
-                            title: "Your work has been saved",
-                            showConfirmButton: false,
-                            timer: 1500
-                          });
-                    }
-                 })
-              })
+const BorrowCard = ({ borrow, deleted, setDeleted }) => {
+  const handleReturn = (id, borrowId) => {
+    fetch(`${import.meta.env.VITE_API_URL}/books/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        fetch(`${import.meta.env.VITE_API_URL}/bookReduce/${id}`, {
+          method: "PATCH",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(data),
         })
-    }
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            console.log(borrowId);
+            fetch(`${import.meta.env.VITE_API_URL}/deleteBorrow/${borrowId}`, {
+              method: "DELETE",
+            })
+              .then((res) => res.json())
+              .then((data) => {
+                console.log(data);
+                if (data.deletedCount === 1) {
+                  Swal.fire({
+                    position: "top",
+                    icon: "success",
+                    title: "Successfully Removed",
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+                }
+                setDeleted(!deleted)
+              });
+          });
+      });
+  };
   return (
     <div>
       <div>
@@ -55,8 +54,11 @@ const BorrowCard = ({ borrow }) => {
             <p>Borrowed Date: {borrow?.borrowDate}</p>
             <p>Return Date: {borrow?.returnDate}</p>
             <div className="card-actions justify-between">
-              <div onClick={() => handleReturn(borrow?.bookId, borrow?._id)} className="w-full text-center rounded-lg px-1 py-2 bg-secondary text-white">
-              Return
+              <div
+                onClick={() => handleReturn(borrow?.bookId, borrow?._id)}
+                className="w-full text-center rounded-lg px-1 py-2 bg-secondary text-white"
+              >
+                Return
               </div>
             </div>
           </div>
@@ -69,5 +71,5 @@ const BorrowCard = ({ borrow }) => {
 export default BorrowCard;
 
 BorrowCard.propTypes = {
-    borrow: PropTypes.object.isRequired
-}
+  borrow: PropTypes.object.isRequired,
+};
--- src/pages/BorrowedBooks.jsx
+++ src/pages/BorrowedBooks.jsx
@@ -5,6 +5,7 @@ import BorrowCard from '../components/BorrowCard';
 const BorrowedBooks = () => {
     const { user } = useContext(AuthContext);
     const [borrows, setBorrows] = useState([])
+    const [deleted, setDeleted] = useState(false)
 
     useEffect(() => {
         fetch(`${import.meta.env.VITE_API_URL}/borrow/${user?.email}`)
@@ -12,14 +13,14 @@ const BorrowedBooks = () => {
            .then(data => {
               setBorrows(data)
            })
-    }, [user?.email])
+    }, [user?.email, deleted])
 
     return (
         <div>
             <h4 className="text-center font-bold text-2xl text-secondary space-y-3">Borrowed Book</h4>
             <div className="grid md:grid-cols-3 gap-4">
                 {
-                    borrows.map(borrow => <BorrowCard key={borrow._id} borrow={borrow}/>)
+                    borrows.map(borrow => <BorrowCard key={borrow._id} borrow={borrow} deleted={deleted} setDeleted={setDeleted}/>)
                 }
             </div>
         </div>
