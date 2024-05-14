43b5450dc3000787fd9ff2d2541a515ba10a35d0
zihad868 <zihadh868@gmail.com>
2024-05-14 08:58:21 +0600

    View Category all Books

--- src/components/CategoriesBooks.jsx
+++ src/components/CategoriesBooks.jsx
@@ -1,10 +1,36 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const CategoriesBooks = ({ book }) => {
+    const {_id, authName, photo, name, quantity, category, description, rating} = book;
     return (
-        <div>
-            <h3>{book.category}</h3>
+        <div  className='space-y-3 mb-10'>
+        <div className='flex justify-center p-3'>
+            <img className='w-96 h-72' src={photo} alt="" />
         </div>
+        <div className='text-xl font-bold ml-12'>
+            <p>Book Name: {name}</p>
+        </div>
+
+        <div className='text-xl font-bold ml-12'>
+            <p>Author Name: {authName}</p> 
+        </div>
+
+        <div className='text-xl font-normal ml-12'>
+            <p>Book Category: {category}</p>
+        </div>
+
+        <div className='grid grid-cols-2 text-xl ml-12'>
+            <p>Quantity: {quantity}</p>
+            <p>Rating: {rating}</p>
+        </div>
+        <div className='ml-12'>
+          <p>Description: {description.slice(0, 30)}{'...'}</p>
+        </div>
+        <div className='w-full'>
+          <Link to={`/books/${_id}`}><button className='bg-sky-300 p-2 rounded-md w-full'>View Details</button></Link>
+        </div>
+    </div>
     );
 };
 
--- src/pages/CategoryDetails.jsx
+++ src/pages/CategoryDetails.jsx
@@ -4,13 +4,14 @@ import CategoriesBooks from "../components/CategoriesBooks";
 const CategoryDetails = () => {
     const books = useLoaderData();
 
-    console.log(books)
     return (
         <div>
             <h3>Category Details{books.length}</h3>
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {
                 books.map(book => <CategoriesBooks key={book._id} book={book} />)
             }
+            </div>
         </div>
     );
 };
