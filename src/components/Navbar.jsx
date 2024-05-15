656694a4a0aed88730bc570dd8faae5240c49914
zihad868 <zihadh868@gmail.com>
2024-05-15 23:11:07 +0600

    Dark and Light Theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from "../assets/logo.png";
 import "../assets/css/Navbar.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 
 const Navbar = () => {
   const { user, signoutUser } = useContext(AuthContext);
+  const [theme, setTheme] = useState('light')
 
-  console.log(user);
+  useEffect(() => {
+    document.querySelector('html').setAttribute('data-theme', theme)
+  }, [theme])
+
+  const handleTheme = (e) => {
+     if(e.target.checked){
+       setTheme('synthwave')
+     } else{
+      setTheme('light')
+     }
+  }
+
 
   const handleSignOut = () => {
     signoutUser();
@@ -63,7 +75,7 @@ const Navbar = () => {
           isActive ? "text-[#70ced3] underline" : ""
         }
       >
-        Books Rating
+        Popular Books
       </NavLink>
     </div>
   );
@@ -108,6 +120,43 @@ const Navbar = () => {
       </div>
 
       <div className="navbar-end">
+        <label className="cursor-pointer grid place-items-center mr-5">
+          <input
+            type="checkbox"
+            onChange={handleTheme}
+            value="synthwave"
+            className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+          />
+          <svg
+            className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
+          <svg
+            className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
         {!user?.email && (
           <>
             <Link
